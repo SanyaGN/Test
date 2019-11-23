79c69f205c014fc3019ed7861818f16128948cfe
Sanya Grigoryan <sgrigoryan@prolevtech.com>
2019-11-23 17:07:57 +0300

    changed hurt

--- src/com/Sanya/Main.java
+++ src/com/Sanya/Main.java
@@ -3,6 +3,6 @@ package com.Sanya;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Don't make me hug you");
+	System.out.println("Don't make me hurt you");
     }
 }
